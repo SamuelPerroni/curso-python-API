956a2879693a378a400dd1d5636429b7bbb5118b
Samuel Perroni <samuel_perroni@outlook.com>
2024-04-15 12:30:52 -0300

    adicionando polimorfismo e abstração

--- modelos/cardapio/item_cardapio.py
+++ modelos/cardapio/item_cardapio.py
@@ -1,4 +1,10 @@
-class ItemCardapio:
+from abc import ABC, abstractclassmethod
+
+class ItemCardapio(ABC):
     def __init__(self, nome: str, preco: float) -> None:
         self._nome = nome
         self._preco = preco
+        
+    @abstractclassmethod
+    def aplicar_desconto(self):
+        ...
